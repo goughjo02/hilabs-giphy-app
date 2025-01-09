654519ab964d577007af71b8ffe3029691b0285e
goughjo02 <quiet.book4435@fastmail.com>
2025-01-09 14:03:24 +0000

    create trending gifs component to decouple app router from component logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-"use client";
-import { ListGifs } from "@/components/giphy/list-gifs";
-import { useListTrending } from "@/hooks/use-list-trending";
+import { TrendingGifs } from "@/components/giphy/trending-gifs";
 
-export default function Home() {
-  const { data, isLoading, hasNextPage, fetchNextPage } = useListTrending();
-  return (
-    <main>
-      <div className="container mx-auto">
-        <h1 className="pt-4 pb-8 px-2 text-2xl ">{"Trending Gifs"}</h1>
-      </div>
-      <ListGifs
-        data={data}
-        isLoading={isLoading}
-        hasNextPage={hasNextPage}
-        fetchNextPage={fetchNextPage}
-      />
-    </main>
-  );
-}
+export default TrendingGifs;
---
+++ components/giphy/trending-gifs.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ListGifs } from "@/components/giphy/list-gifs";
+import { useListTrending } from "@/hooks/use-list-trending";
+
+export function TrendingGifs() {
+  const { data, isLoading, hasNextPage, fetchNextPage } = useListTrending();
+  return (
+    <main>
+      <div className="container mx-auto">
+        <h1 className="pt-4 pb-8 px-2 text-2xl ">{"Trending Gifs"}</h1>
+      </div>
+      <ListGifs
+        data={data}
+        isLoading={isLoading}
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+      />
+    </main>
+  );
+}
